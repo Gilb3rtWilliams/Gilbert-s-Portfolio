cb993f238c6b0ccb140198e7e16941930db5363c
Gilbert Williams <lance6827@gmail.com>
2025-07-03 09:09:30 +0300

    A few alterations

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const images = [
     'images/image1.jpg',
     'images/image2.jpg',
     'images/image3.jpg',
-    'images/image4.jpg'
+    'images/image4.jpg',
+    'images/image5.jpg',
 ];
 
 let currentIndex = 0;
